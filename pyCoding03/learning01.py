67cb379ddd080f3d4675b8b59b80c60e9423b745
hyeon-blip <jshgoogol@gmail.com>
2024-09-12 14:45:22 +0900

    Add files via upload

---
+++ pyCoding03/learning01.py
@@ -0,0 +1,12 @@
+# 리스트
+# tempList = ['안녕!',10,20,40,50]
+# print(tempList[-1])
+
+# tempList = [[10,20,30],[40,50,60]]
+# print(tempList[1][1])
+
+tempList = [10,20,30]
+tempList[0] = 1
+tempList[1] = 2
+tempList[2] = 3
+print('변경후 :',tempList)
