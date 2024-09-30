8a68c4fe7c87d75b69a705f958117917d55952f5
hyeon-blip <jshgoogol@gmail.com>
2024-09-30 15:47:12 +0900

    Add files via upload

--- pyCoding04/learning01.py
+++ pyCoding04/learning01.py
@@ -25,9 +25,22 @@
 #   for j in range(1,10,1):
 #     print(i,'*',j,'=',i*j)
     
-a = ['바나나','사과','배','딸기']
-for i in a:
-  print(i)
-  if i == '배':
-    print('배 찾음')
-    break
+# a = ['바나나','사과','배','딸기']
+# for i in a:
+#   print(i)
+#   if i == '배':
+#     print('배 찾음')
+#     break
+
+# a = ['사과','딸기','배','두리안','귤']
+# for i, v in enumerate(a) :
+#   if v == '배':
+#     a[i] = 0
+#     break
+# print(a)
+
+aaa = [[1,2,3],[4,5,6],[7,8,9]]
+for x in aaa:
+  print(x)
+  for y in x:
+    print(y)
