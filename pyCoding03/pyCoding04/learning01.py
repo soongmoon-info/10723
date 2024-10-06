d7068fffa3004257c3f1af3c134134842acb9b2f
hyeon-blip <jshgoogol@gmail.com>
2024-10-06 19:58:47 +0900

    Add files via upload

---
+++ pyCoding03/pyCoding04/learning01.py
@@ -0,0 +1,46 @@
+#반복문
+# i = 0
+# while True:
+#   print(i)
+#   i = i+1
+
+# for i in range(0,3,1):
+#   print(i)
+  
+# a = [50,40,10,20,30]
+# for i in  a:
+#   print(i)
+
+# i = 0
+# while i < 3:
+#   print(i)
+#   j = 0
+#   while j < 3:
+#     print(i,j)
+#     j = j+1
+#   i = i+1
+  
+# for i in range(2,10,1):
+#   print(i,'단')
+#   for j in range(1,10,1):
+#     print(i,'*',j,'=',i*j)
+    
+# a = ['바나나','사과','배','딸기']
+# for i in a:
+#   print(i)
+#   if i == '배':
+#     print('배 찾음')
+#     break
+
+# a = ['사과','딸기','배','두리안','귤']
+# for i, v in enumerate(a) :
+#   if v == '배':
+#     a[i] = 0
+#     break
+# print(a)
+
+# aaa = [[1,2,3],[4,5,6],[7,8,9]]
+# for x in aaa:
+#   print(x)
+#   for y in x:
+#     print(y)
