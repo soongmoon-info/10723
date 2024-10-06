04dfc993208185b5a8c4f0c6e2091f71f565b061
hyeon-blip <jshgoogol@gmail.com>
2024-10-06 18:44:12 +0900

    Add files via upload

--- pyCoding02/exam01.py
+++ pyCoding02/exam01.py
@@ -1,8 +1,8 @@
 score = float(input('수학점수를 입력하시오:'))
 score = float(input('정보점수를 입력하시오:'))+score
 score = float(input('국어점수를 입력하시오:'))+score
-print('총합점수',score)
-print('평균점수',score//3)
+print('총합:',score)
+print('평균:',score//3)
 average = score/3
 if average >= 90:
   print('등급 : A')
--- pyCoding03/exam02.py
+++ pyCoding03/exam02.py
@@ -5,29 +5,43 @@ student[1].remove('none')
 
 student.append([input('학생 이름 입력:'),input('학생 학번 입력:'),float(input('수학 점수 입력:')),float(input('국어 점수 입력:')),float(input('정보 점수 입력:'))])
 
-student[0].append(student[0][2]+student[0][3]+student[0][4])
-student[1].append(student[1][2]+student[1][3]+student[1][4])
-student[2].append(student[2][2]+student[2][3]+student[2][4])
+# 합 추가
 
+# student[0].append(student[0][2]+student[0][3]+student[0][4])
+# student[1].append(student[1][2]+student[1][3]+student[1][4])
+# student[2].append(student[2][2]+student[2][3]+student[2][4])
+
+student[0].append(sum(student[0][2:5]))
+student[1].append(sum(student[1][2:5]))
+student[2].append(sum(student[2][2:5]))
+
+# 평균
 student[0].append(int(student[0][5]/3))
 student[1].append(int(student[1][5]/3))
 student[2].append(int(student[2][5]/3))
 
-if student[0][6] >= 90:
+if student[0][2] < 30 or student[0][3] < 30 or student[0][4]<30:
+ student[0].insert(2,'F')
+elif student[0][6] >= 90:
   student[0].insert(2,'A')
 elif student[0][6] >= 80:
   student[0].insert(2,'B')
 else:
   student[0].insert(2,'C')
   
-if student[1][6] >= 90:
+if student[1][2] < 30 or student[1][3] < 30 or student[1][4]<30:
+ student[1].insert(2,'F')
+elif student[1][6] >= 90:
   student[1].insert(2,'A')
 elif student[1][6] >= 80:
   student[1].insert(2,'B')
 else:
   student[1].insert(2,'C')
 
-if student[2][6] >= 90:
+
+if student[2][2] < 30 or student[2][3] < 30 or student[2][4]<30:
+ student[2].insert(2,'F')
+elif student[2][6] >= 90:
   student[2].insert(2,'A')
 elif student[2][6] >= 80:
   student[2].insert(2,'B')
